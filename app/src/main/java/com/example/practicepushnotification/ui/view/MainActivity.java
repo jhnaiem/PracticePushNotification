b060f917f337d0c051221f19cec54e01546249c1
Jahid Hasan Naiem <naiem@field.buzz>
2021-02-12 08:38:19 +0600

    Implement realtime update

--- app/src/main/java/com/example/practicepushnotification/ui/view/MainActivity.java
+++ app/src/main/java/com/example/practicepushnotification/ui/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.practicepushnotification.ui.view;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +14,15 @@ import com.example.practicepushnotification.R;
 import com.example.practicepushnotification.data.model.Contact;
 import com.example.practicepushnotification.ui.adapter.RecyclerAdapter;
 import com.example.practicepushnotification.ui.viewModel.MainActivityViewModel;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -22,18 +32,22 @@ import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG =MainActivity.class.getName() ;
     private RecyclerView recyclerView;
     private List<Contact> contactList;
 
     private RecyclerAdapter contactAdapter;
     private MainActivityViewModel mainActivityViewModel;
 
+    private FirebaseFirestore firebaseDatabase = FirebaseFirestore.getInstance();
+    private CollectionReference phoneBookRef = firebaseDatabase.collection("phonebook");
     private Realm mRealm = null;
 
     @Override
@@ -67,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                         if (permissionGrantedResponse.getPermissionName().equals(Manifest.permission.READ_CONTACTS)) {
                             contactList.addAll(mainActivityViewModel.getContacts());
                             Log.d("===>", " ContactPassed: " + contactList.size());
-                            mainActivityViewModel.writeinFirebase();
+                            mainActivityViewModel.writeinFirebase(firebaseDatabase);
                             contactAdapter.notifyDataSetChanged();
                         }
                     }
@@ -84,6 +98,55 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }).check();
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        phoneBookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null){
+                    return;
+                }
+
+                      Contact storeContact;
+
+                for (DocumentChange changeItr : value.getDocumentChanges()){
+
+
+                    DocumentSnapshot documentSnapshot = changeItr.getDocument();
+
+                    String id = documentSnapshot.getId();
+                    int oldIndex = changeItr.getOldIndex();
+                    int newIndex = changeItr.getNewIndex();
+                    switch (changeItr.getType()) {
+                        case ADDED:
+
+                            storeContact = new Contact();
+                            storeContact.setId(id);
+                            storeContact.setName(documentSnapshot.getString("Name"));
+                            storeContact.setName(documentSnapshot.getString("Number"));
+
+                            contactList.add(newIndex,storeContact);
+
+                            break;
+                        case MODIFIED:
+
+                            break;
+                        case REMOVED:
+
+                            break;
+                    }
+                }
+
+            }
+        });
 
 
     }
